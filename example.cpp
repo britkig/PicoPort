03b2b236b693894ee8efc91c25cddf872537899a
BritKig <97478354+britkig@users.noreply.github.com>
2025-11-12 10:29:01 +0000

    Update example.cpp

--- example.cpp
+++ example.cpp
@@ -1,19 +1,16 @@
-
 //  EXAMPLE PROJECT
-//  Flashes on-module Pi Pico LED at 1Hz intervals
+//  Flashes the on-module Pi Pico LED at 1Hz intervals whilst GPIO pin 0 is being pulled to GND.
 
+#include "pin_input.h"
 #include "pin_output.h"
 
+PinInput Button0(0);
 PinOutput BuiltInLED(LED_BUILTIN);
 
 void main(){
-
 	while(true){
-
+		Button0.WaitForState(true);
 		BuiltInLED.Set(!BuiltInLED.Get());
-
 		sleep_ms(1000);
-
-  }
-
+	}
 }
