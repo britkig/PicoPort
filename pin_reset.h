010c5a7602516353a61e3786823220268a5fab24
BritKig <97478354+britkig@users.noreply.github.com>
2025-11-12 10:10:27 +0000

    Create pin_reset.h

---
+++ pin_reset.h
@@ -0,0 +1,13 @@
+#pragma once	// FOR PI PICO ONLY
+#include "pin_input.h"
+struct PinReset : PinInput{
+	PinReset(uint8_t p):PinInput(p,false){
+		WaitForState(false);
+		gpio_set_irq_enabled_with_callback(_p, GPIO_IRQ_EDGE_FALL, true, &_trigger);
+	}
+private:
+	static void _trigger(uint gpio, uint32_t events){
+		watchdog_enable(0,1);
+		while(true);
+	}
+};
