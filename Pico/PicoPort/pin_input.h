fb48c3af0a4f50552fc8ef12da42c66bde52920c
BritKig <97478354+britkig@users.noreply.github.com>
2025-11-13 19:41:44 +0000

    Update pin_input.h

--- Pico/PicoPort/pin_input.h
+++ Pico/PicoPort/pin_input.h
@@ -1,22 +1,25 @@
-#pragma once	// FOR PI PICO ONLY
+#pragma once  // FOR PI PICO ONLY
 #include "pin.h"
 struct PinInput : Pin{
-	void subscribe_irq(bool e){
-		gpio_set_irq_enabled(_p,GPIO_IRQ_EDGE_RISE|GPIO_IRQ_EDGE_FALL,e);
-	}
-	PinInput(uint8_t p,bool irq=true):Pin(p){
-		gpio_set_dir(_p,GPIO_IN);
-		gpio_pull_up(_p);
-		if(irq) subscribe_irq(irq);
-	}
-	bool Get(){
-		return !gpio_get(_p);
-	}
-	void WaitForState(bool s=true){
-		while(Get()!=s) __wfi();
-	}
-	void WaitForPress(bool s=true){
-		WaitForState(!s);
-		WaitForState(s);
-	}
+private:
+  static void _noop(uint gpio, uint32_t events){}
+public:
+  PinInput(uint8_t p,bool irq=true):Pin(p){
+    gpio_set_dir(_p,GPIO_IN);
+    gpio_pull_up(_p);
+    if(irq){
+       irq_set_enabled(IO_IRQ_BANK0,true);
+       gpio_set_irq_enabled_with_callback(_p,GPIO_IRQ_EDGE_RISE|GPIO_IRQ_EDGE_FALL,true,&_noop);
+    }
+  }
+  bool Get(){
+    return !gpio_get(_p);
+  }
+  void WaitForState(bool s=true){
+    while(Get()!=s) __wfi();
+  }
+  void WaitForPress(bool s=true){
+    WaitForState(!s);
+    WaitForState(s);
+  }
 };
